6f11f424b6fdf0b6de00e65e6400dbf817275179
Yousef Mostafa <115595202+YASOOGX@users.noreply.github.com>
2026-03-13 08:34:08 +0200

    Add files via upload

---
+++ service.php
@@ -0,0 +1,92 @@
+<?php get_header(); the_post();
+/*
+*Template Name: خدماتنا
+*/
+?>
+  <!-- ////////////////////////////////// -->
+   <style>
+    .service {
+    }
+
+    .service .card {
+      border-radius: 20px;
+      width: 100%;
+      box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+    }
+    .service  .col {
+      padding: 0px;
+      margin: 20px 0.5%;
+    }
+    .service .card img {
+      border-radius: 20px 20px 0 0;
+      object-fit: cover;
+      height: 270px ;
+    }
+
+    .service .card-title {
+      font-size: 24px;
+      font-weight: bold;
+    }
+
+    .service .card-text {
+      font-size: 16px;
+    }
+   </style>
+  <div class="service mt-150-px">
+    <div class="container">
+      <div class="row row-cols-lg-3 row-cols-md-2 row-cols-sm-1">
+        <div class="col">
+          <div class="card" >
+            <img class="card-img-top" src="assets/image/who-us-back.png" alt="Card image">
+            <div class="card-body">
+              <h4 class="card-title">العنوان</h4>
+              <p class="card-text mb-3">هذا النص يمكن استبداله هذا النص يمكن استبداله هذا النص يمكن استبداله هذا النص يمكن استبداله هذا النص يمكن استبداله</p>
+              <a href="#" class="btn btn-primary">المزيد</a>
+            </div>
+          </div>
+        </div>
+                <div class="col">
+          <div class="card" >
+            <img class="card-img-top" src="assets/image/who-us-back.png" alt="Card image">
+            <div class="card-body">
+              <h4 class="card-title">العنوان</h4>
+              <p class="card-text mb-3">هذا النص يمكن استبداله هذا النص يمكن استبداله هذا النص يمكن استبداله هذا النص يمكن استبداله هذا النص يمكن استبداله</p>
+              <a href="#" class="btn btn-primary">المزيد</a>
+            </div>
+          </div>
+        </div>
+                <div class="col">
+          <div class="card" >
+            <img class="card-img-top" src="assets/image/who-us-back.png" alt="Card image">
+            <div class="card-body">
+              <h4 class="card-title">العنوان</h4>
+              <p class="card-text mb-3">هذا النص يمكن استبداله هذا النص يمكن استبداله هذا النص يمكن استبداله هذا النص يمكن استبداله هذا النص يمكن استبداله</p>
+              <a href="#" class="btn btn-primary">المزيد</a>
+            </div>
+          </div>
+        </div>
+                <div class="col">
+          <div class="card" >
+            <img class="card-img-top" src="assets/image/who-us-back.png" alt="Card image">
+            <div class="card-body">
+              <h4 class="card-title">العنوان</h4>
+              <p class="card-text mb-3">هذا النص يمكن استبداله هذا النص يمكن استبداله هذا النص يمكن استبداله هذا النص يمكن استبداله هذا النص يمكن استبداله</p>
+              <a href="#" class="btn btn-primary">المزيد</a>
+            </div>
+          </div>
+        </div>
+                <div class="col">
+          <div class="card" >
+            <img class="card-img-top" src="assets/image/who-us-back.png" alt="Card image">
+            <div class="card-body">
+              <h4 class="card-title">العنوان</h4>
+              <p class="card-text mb-3">هذا النص يمكن استبداله هذا النص يمكن استبداله هذا النص يمكن استبداله هذا النص يمكن استبداله هذا النص يمكن استبداله</p>
+              <a href="#" class="btn btn-primary">المزيد</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <!-- ////////////////////////////////////////////////////// -->
+<?php get_footer(); ?>
